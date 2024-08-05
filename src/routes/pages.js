464bd396481ae863ed92bb1c5e98298f26476f7d
Ali Haroon <112351004+Ali-Haroon3@users.noreply.github.com>
2024-08-04 23:10:22 -0600

    Changed header and footer into partials, added some pages
    Still need to fill out Legacy, Contact, Leadership, and Sponsorship pages. Eventually will have to put everything in one big views folder but that was messing up routes and stuff so do that last

--- src/routes/pages.js
+++ src/routes/pages.js
@@ -4,27 +4,47 @@ const path = require('path');
 const authMiddleware = require('../middleware/authMiddleware');
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../index.html'));
+    res.sendFile(path.join(__dirname, '../../views/index.html'));
 });
 
 router.get('/about.html', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../about.html'));
+    res.sendFile(path.join(__dirname, '../../views/about.html'));
 });
 
 router.get('/login.html', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../login.html'));
+    res.sendFile(path.join(__dirname, '../../views/login.html'));
 });
 
 router.get('/register.html', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../register.html'));
+    res.sendFile(path.join(__dirname, '../../views/register.html'));
 });
 
 router.get('/points.html', authMiddleware, (req, res) => {
-    res.sendFile(path.join(__dirname, '../../points.html'));
+    res.sendFile(path.join(__dirname, '../../views/points.html'));
 });
 
 router.get('/calendar.html', authMiddleware, (req, res) => {
-    res.sendFile(path.join(__dirname, '../../calendar.html'));
+    res.sendFile(path.join(__dirname, '../../views/calendar.html'));
+});
+
+router.get('/contact.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../views/contact.html'));
+});
+
+router.get('/sponsorship.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../views/sponsorship.html'));
+});
+
+router.get('/leadership.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../views/leadership.html'));
+});
+
+router.get('/legacy.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../views/legacy.html'));
+});
+
+router.get('/portal.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../views/portal.html'));
 });
 
 module.exports = router;
