0b381966e7d736318db8b14f1b05ef6aa0fb4c75
Ali Haroon <112351004+Ali-Haroon3@users.noreply.github.com>
2024-08-17 20:04:08 -0600

    Testing moving files to remove .html at the end of github pages

--- src/routes/pages.js
+++ src/routes/pages.js
@@ -7,43 +7,43 @@ router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../../views/index.html'));
 });
 
-router.get('/about.html', (req, res) => {
+router.get('/about', (req, res) => {
     res.sendFile(path.join(__dirname, '../../views/about.html'));
 });
 
-router.get('/login.html', (req, res) => {
+router.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, '../../views/login.html'));
 });
 
-router.get('/register.html', (req, res) => {
+router.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, '../../views/register.html'));
 });
 
-router.get('/points.html', authMiddleware, (req, res) => {
+router.get('/points', authMiddleware, (req, res) => {
     res.sendFile(path.join(__dirname, '../../views/points.html'));
 });
 
-router.get('/calendar.html', authMiddleware, (req, res) => {
+router.get('/calendar', authMiddleware, (req, res) => {
     res.sendFile(path.join(__dirname, '../../views/calendar.html'));
 });
 
-router.get('/contact.html', (req, res) => {
+router.get('/contact', (req, res) => {
     res.sendFile(path.join(__dirname, '../../views/contact.html'));
 });
 
-router.get('/sponsorship.html', (req, res) => {
+router.get('/sponsorship', (req, res) => {
     res.sendFile(path.join(__dirname, '../../views/sponsorship.html'));
 });
 
-router.get('/leadership.html', (req, res) => {
+router.get('/leadership', (req, res) => {
     res.sendFile(path.join(__dirname, '../../views/leadership.html'));
 });
 
-router.get('/legacy.html', (req, res) => {
+router.get('/legacy', (req, res) => {
     res.sendFile(path.join(__dirname, '../../views/legacy.html'));
 });
 
-router.get('/portal.html', (req, res) => {
+router.get('/portal', (req, res) => {
     res.sendFile(path.join(__dirname, '../../views/portal.html'));
 });
 
