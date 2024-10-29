45df3f39645a4a6b7f10101ef34dc8674be14ebd
Ali Haroon <112351004+Ali-Haroon3@users.noreply.github.com>
2024-10-29 00:48:28 -0600

    tryna fix points.html

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,28 +1,3 @@
-// // src/middleware/authMiddleware.js
-// const jwt = require('jsonwebtoken');
-// const dotenv = require('dotenv');
-
-// dotenv.config();
-
-// const authMiddleware = (req, res, next) => {
-//     const authHeader = req.headers['authorization'];
-//     const token = authHeader && authHeader.split(' ')[1];
-
-//     if (!token) {
-//         return res.status(401).json({ message: 'Access token missing' });
-//     }
-
-//     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-//         if (err) {
-//             return res.status(403).json({ message: 'Invalid token' });
-//         }
-//         req.userId = user.id; // Assuming the JWT payload contains the user ID as 'id'
-//         next();
-//     });
-// };
-
-// module.exports = authMiddleware;
-// src/middleware/authMiddleware.js
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 
